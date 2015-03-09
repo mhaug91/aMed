730c3291e4aada3e16081ba57faa9b51b45966b3
Mesje <k.myhre@live.no>
2015-03-09 13:14:26 +0100

    Lagt til controller

---
+++ aMed/finnBehandlerTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  finnBehandlerTableViewController.h
+//  aMed
+//
+//  Created by Kristofer Myhre on 09/03/15.
+//  Copyright (c) 2015 MacBarhaug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface finnBehandlerTableViewController : UITableViewController
+
+@end
