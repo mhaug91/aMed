a17d962f8df49b3ef2ddc40f17e912f7a475d8f9
mhaug91 <mhaug91@gmail.com>
2015-03-09 12:35:22 +0100

    Initial Commit

---
+++ aMed/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  aMed
+//
+//  Created by MacBarhaug on 09.03.15.
+//  Copyright (c) 2015 MacBarhaug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
