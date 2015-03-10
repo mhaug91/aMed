34e31557a58f997d81dbee8feac95b69ed340294
mhaug91 <mhaug91@gmail.com>
2015-03-10 14:49:12 +0100

    justerte nyhetsview

--- aMed/NyhetsViewController.m
+++ aMed/NyhetsViewController.m
@@ -10,6 +10,8 @@
 
 @interface NyhetsViewController ()
 
+@property (copy, nonatomic) NSArray *dwarves;
+
 @end
 
 @implementation NyhetsViewController
@@ -17,6 +19,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"Hello Myhre");
+    self.dwarves = @[@"Sleepy", @"Sneezy", @"Bashful", @"Happy",
+                     @"Doc", @"Grumpy", @"Dopey",
+                     @"Thorin", @"Dorin", @"Nori", @"Ori",
+                     @"Balin", @"Dwalin", @"Fili", @"Kili",
+                     @"Oin", @"Gloin", @"Bifur", @"Bofur",
+                     @"Bombur"];
+    UITableView *tableView = (id)[self.view viewWithTag:1];
+    UIEdgeInsets contentInset = tableView.contentInset;
+    contentInset.top = 94;
+    [tableView setContentInset:contentInset];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -24,5 +36,33 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma marks
+- (NSInteger)tableView:(UITableView *)tableView
+ numberOfRowsInSection:(NSInteger)section
+{
+    return [self.dwarves count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *SimpleTableIdentifier = @"SimpleTableIdentifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:
+                             SimpleTableIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]
+                initWithStyle:UITableViewCellStyleDefault
+                reuseIdentifier:SimpleTableIdentifier];
+    }
+    UIImage *image = [UIImage imageNamed:@"star"];
+    cell.imageView.image = image;
+    UIImage *image2 = [UIImage imageNamed:@"star3"];
+    cell.imageView.highlightedImage = image2;
+    cell.textLabel.text = self.dwarves[indexPath.row];
+    
+    return cell;
+}
+
 
 @end
