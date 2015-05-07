568c04bfeefb62556b53a3e1a4cdc96a1f4fd4e2
mhaug91 <mhaug91@gmail.com>
2015-05-07 14:33:43 +0200

    calendar controller

--- aMed/CalendarViewController.h
+++ aMed/CalendarViewController.h
@@ -8,14 +8,28 @@
 
 #import <UIKit/UIKit.h>
 #import <JTCalendar/JTCalendar.h>
+#import "RetrieveData.h"
+#import "SelectedEventViewController.h"
 
 
 
-@interface CalendarViewController : UIViewController
+@interface CalendarViewController : UIViewController<JTCalendarDataSource>
 
 @property (weak, nonatomic) IBOutlet JTCalendarMenuView *calendarMenuView;
 
 @property (weak, nonatomic) IBOutlet JTCalendarContentView *calendarContentView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *calendarContentViewHeight;
+@property (weak, nonatomic) NSString *eventID;// = @"eventID";
+//@property (strong, nonatomic) NSInteger COURSE;// = 66; // blue
+//@property (weak, nonatomic) NSInteger FESTIVAL;// = 71; //red
+//@property (weak, nonatomic) NSInteger EXHIBITION;// = 70; //green
+//@property (weak, nonatomic) NSInteger EXHIBITION_2;// = 86; //green
+
+
+@property(strong, nonatomic) RetrieveData *rd;
+@property(strong, nonatomic) NSMutableArray *jsonArray;
+@property(strong, nonatomic) NSMutableArray *eventArray;
+@property(strong, nonatomic) NSMutableArray *filterArray;
 
 
 
--- aMed/CalendarViewController.m
+++ aMed/CalendarViewController.m
@@ -8,28 +8,52 @@
 
 #import "CalendarViewController.h"
 
+
+
 @interface CalendarViewController ()
 
+@property (nonatomic, assign) NSInteger eventIndex;
+
 @end
+
+#define getEventsURL @"http://www.amed.no/AmedApplication/getEvents.php"
+
+
 
 @implementation CalendarViewController
 
 - (void)viewDidLoad
-
 {
+    self.eventIndex = 0;
+    @try {
+        self.rd = [[RetrieveData alloc] init];
+        self.eventArray = [self.rd retrieveEvents];
+    }
+    @catch (NSException *exception) {
+    }
     
     [super viewDidLoad];
-    
+    UIButton *infoButton = [UIButton buttonWithType:UIButtonTypeInfoLight];
+    [infoButton addTarget:self action:@selector(infoPressed:) forControlEvents:UIControlEventTouchUpInside];
+    UIBarButtonItem *infoButtonItem = [[UIBarButtonItem alloc] initWithCustomView:infoButton];
+    self.navigationItem.rightBarButtonItem = infoButtonItem;
+
     
     
     self.calendar = [JTCalendar new];
     
+    // All modifications on calendarAppearance have to be done before setMenuMonthsView and setContentView
+    // Or you will have to call reloadAppearance
+    {
+        self.calendar.calendarAppearance.calendar.firstWeekday = 2; // Sunday == 1, Saturday == 7
+        self.calendar.calendarAppearance.dayCircleRatio = 9. / 10.;
+        self.calendar.calendarAppearance.ratioContentMenu = 1.;
+    }
     
     [self.calendar setMenuMonthsView:self.calendarMenuView];
-    
     [self.calendar setContentView:self.calendarContentView];
-    
     [self.calendar setDataSource:self];
+
 }
 
 
@@ -37,6 +61,23 @@
 
 {
     
+    @try {
+        if (self.eventArray.count == 0) {
+            self.rd = [[RetrieveData alloc] init];
+            self.eventArray = [self.rd retrieveEvents];
+        }
+    }
+    @catch (NSException *exception) {
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Ingen tilgang til nettverk"
+                                                            message:@"Slå på nettverk inne på innstillinger for å få tilgang til innhold" delegate:self
+                                                  cancelButtonTitle:@"Ok" otherButtonTitles:@"Innstillinger", nil];
+        [alertView show];
+        NSLog(@"Exception:s %@", exception.reason);
+    }
+    @finally {
+        [self.view setNeedsDisplay];
+    }
+
     [super viewDidAppear:animated];
     
     
@@ -45,13 +86,82 @@
     
 }
 
+- (void) alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if( 0 == buttonIndex ){ //cancel button
+        [alertView dismissWithClickedButtonIndex:buttonIndex animated:YES];
+    } else if ( 1 == buttonIndex ){
+        [alertView dismissWithClickedButtonIndex:buttonIndex animated:YES];
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];
+        
+    }
+}
+
+
+- (IBAction)didGoTodayTouch
+{
+    [self.calendar setCurrentDate:[NSDate date]];
+}
+
+- (IBAction)didChangeModeTouch
+{
+    self.calendar.calendarAppearance.isWeekMode = !self.calendar.calendarAppearance.isWeekMode;
+    
+    [self transitionExample];
+}
+
+
+- (void)transitionExample
+{
+    CGFloat newHeight = 300;
+    if(self.calendar.calendarAppearance.isWeekMode){
+        newHeight = 75.;
+    }
+    
+    [UIView animateWithDuration:.5
+                     animations:^{
+                         self.calendarContentViewHeight.constant = newHeight;
+                         [self.view layoutIfNeeded];
+                     }];
+    
+    [UIView animateWithDuration:.25
+                     animations:^{
+                         self.calendarContentView.layer.opacity = 0;
+                     }
+                     completion:^(BOOL finished) {
+                         [self.calendar reloadAppearance];
+                         
+                         [UIView animateWithDuration:.25
+                                          animations:^{
+                                              self.calendarContentView.layer.opacity = 1;
+                                          }];
+                     }];
+}
+
+
 
 
 - (BOOL)calendarHaveEvent:(JTCalendar *)calendar date:(NSDate *)date
 
 {
+    Events *e = nil;
+    e = [self.eventArray objectAtIndex:self.eventIndex];
     
-    return NO;
+    // *dateString = e.start_date;
+    NSString *dateString = [e.start_date substringToIndex:10];
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"yyyy-MM-dd"];
+    NSString *stringFromDate = [formatter stringFromDate:date];
+    NSString *eksDate = @"2015-02-26";
+    if([stringFromDate isEqualToString:dateString]){
+        if(self.eventIndex < self.eventArray.count-1){
+            self.eventIndex++;
+            
+        }
+        return YES;
+    }else{
+        return NO;
+    }
     
 }
 
@@ -61,19 +171,36 @@
 
 {
     
-    NSLog(@"%@", date);
+    if([self calendarHaveEvent:calendar date:date]){
+        NSLog(@"Date: %@", date);
+        [self performSegueWithIdentifier:@"pushSelectedDate" sender:date];
+        
+    }
     
 }
 
+- (void) infoPressed:(id) sender{
+    NSString *title = @"Kalender";
+    NSString *info = @"Arrangements kalender";
+    [[[UIAlertView alloc] initWithTitle:title message:info delegate:nil cancelButtonTitle:nil otherButtonTitles:@"Ok", nil] show];
+}
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
+    if([[segue identifier] isEqualToString:@"pushSelectedDate"]){
+        Events *event = nil;
+        event = [self.eventArray objectAtIndex:0];
+
+        SelectedEventViewController *destViewController = segue.destinationViewController; // Getting new view controller
+        destViewController.navigationItem.title = @"event"; // Setting title in the navigation bar of next view
+        [destViewController getEventObject:event];
+        
+    }
 }
-*/
 
 @end
